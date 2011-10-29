4d0fc0b52842da0e3a021608da7c88906fdaec6a
Papadopoulos Nikos <nikpapas@gmail.com>
2011-10-29 05:49:52 +0100

    Added visual studio 2010 project files

---
+++ vs/dllmain.cpp
@@ -0,0 +1,49 @@
+/*
+
+    This file is part of the nemesis math library.
+
+    dllmain.cpp
+    dll entry point
+
+    Copyright (C) 2008, 2010, 2011
+    Papadopoulos Nikolaos
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General
+    Public License along with this library; if not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301 USA
+
+*/
+
+
+#include <windows.h>
+
+#pragma warning( push ) 
+#pragma warning( disable : 4100 ) // unreferenced formal parameter
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			// For optimization.
+			DisableThreadLibraryCalls( hModule );
+			break;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+
+	return TRUE;
+}
