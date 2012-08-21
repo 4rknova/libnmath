d196a9e8664ffdc1df28696eccd138391b789d58
Papadopoulos Nikos <nikpapas@gmail.com>
2012-08-21 11:56:55 +0100

    Fixed bugs in interpolation methods

--- src/interpolation.h
+++ src/interpolation.h
@@ -27,7 +27,6 @@
 
 /*
     TO DO:
-	- Smooth step
     - TCB spline (Kochanek-Bartels)
     - Beta splines
     - Uniform nonrational splines
@@ -46,34 +45,43 @@ namespace NMath {
 extern "C" {
 #endif	/* __cplusplus */
 
+/* 
+	Smoothstep
+
+	Notes:
+	The inputs are values edge0, edge1, x with edge0 < x < edge1.
+	Unlike the rest interpolation methods, the returned value 
+	for smoothstep is in the 0 - 1 range.
+*/
+static inline scalar_t smoothstep(const scalar_t edge0, const scalar_t edge1, const scalar_t x);
+static inline scalar_t smoothstep_perlin(const scalar_t edge0, const scalar_t edge1, const scalar_t x);
+
 /* Simple */
-static inline scalar_t step(const scalar_t a, const scalar_t b, const scalar_t p);
-static inline scalar_t linear(const scalar_t a, const scalar_t b, const scalar_t p);
-static inline scalar_t sine(const scalar_t a, const scalar_t b, const scalar_t p);
-static inline scalar_t cosine(const scalar_t a, const scalar_t b, const scalar_t p);
+static inline scalar_t step(const scalar_t a, const scalar_t b, const scalar_t t);
+static inline scalar_t linear(const scalar_t a, const scalar_t b, const scalar_t t);
+static inline scalar_t cosine(const scalar_t a, const scalar_t b, const scalar_t t);
+static inline scalar_t acceleration(const scalar_t a, const scalar_t b, const scalar_t t);
+static inline scalar_t deceleration(const scalar_t a, const scalar_t b, const scalar_t t);
 
-/* Polynomial */
-static inline scalar_t acceleration(const scalar_t a, const scalar_t b, const scalar_t p);
-static inline scalar_t deceleration(const scalar_t a, const scalar_t b, const scalar_t p);
-static inline scalar_t cubic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t p);
+/* 
+	Splines
 
-/* Splines */
-/*
 	Notes:
-	if multiples points are given then we are interpolating between the 2 middle ones.
-	t denotes tangent.
+	We are interpolating between the 2 middle values.
 */
-static inline scalar_t hermite(const scalar_t t1, const scalar_t a, const scalar_t b, const scalar_t t2, const scalar_t p);
-static inline scalar_t cardinal(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t t, const scalar_t p);
-static inline scalar_t catmullrom(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t p);
+static inline scalar_t cubic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t t);
+static inline scalar_t hermite(const scalar_t tang1, const scalar_t a, const scalar_t b, const scalar_t tang2, const scalar_t t);
+static inline scalar_t cardinal(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t p, const scalar_t t);
+static inline scalar_t catmullrom(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t t);
 
-/* Bezier */
 /*
+	Bezier
+
 	Notes:
-	We are interpolating between the first and last points.
+	We are interpolating between the first and last values.
 */
-static inline scalar_t bezier_quadratic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t p);				/* DeCasteljau */
-static inline scalar_t bezier_cubic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t p);	/* DeCasteljau */
+static inline scalar_t bezier_quadratic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t t);
+static inline scalar_t bezier_cubic(const scalar_t a, const scalar_t b, const scalar_t c, const scalar_t d, const scalar_t t);
 
 #ifdef __cplusplus
 }   /* extern "C" */
